14b0845e653b4779144dfdda47b062ca35296d32
hbsoftco <hossein.bajan@gmail.com>
2020-01-03 18:40:34 +0330

    Update server file

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const Sequelize = require('sequelize');
-const dotenv = require('dotenv').config();
-
 const app = express();
 const port = process.env.DB_PORT;
+
+const Sequelize = require('sequelize');
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv').config();
+
+// on the request to root (localhost:3000/)
+app.get('/', function (req, res) {
+    res.send('<b>My</b> first express http server');
+});
+
+// On localhost:3000/welcome
+app.get('/welcome', function (req, res) {
+    res.send('<b>Hello</b> welcome to my http server made with express');
+});
+
+// Change the 404 message modifing the middleware
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
+
 
 // Controllers
 const RateController = require('./app/Controllers/RateController')
@@ -11,8 +29,30 @@ const bookController = new RateController();
 bookController.index('test');
 
 class Server {
+
     constructor() {
 
+        this.initDB();
+        this.initExpressMiddleware();
+        this.initRoutes();
+        this.start();
+
+    }
+
+    start() {
+        // Creating an express http server
+        app.listen(port, () => console.log(`App listening on port ${port}!`));
+    }
+
+    initExpressMiddleware() {
+        // Attaching body parser middlware
+        // parse application/x-www-form-urlencoded
+        app.use(bodyParser.urlencoded({ extended: false }))
+        // parse application/json
+        app.use(bodyParser.json())
+    }
+
+    initDB() {
         const sequelize = new Sequelize(
             process.env.DB_DATABASE,
             process.env.DB_USERNAME,
@@ -21,8 +61,9 @@ class Server {
                 host: process.env.DB_HOST,
                 dialect: process.env.DB_CONNECTION
             });
-
     }
+
+    initRoutes() { }
 
 }
 
